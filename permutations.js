0b2e60d4cc97ae7770af28f56dfd058b0dd88cf8
samuelahmed <samueladuval@gmail.com>
2022-05-02 17:21:48 -0700

    Mon May  2 17:21:27 PDT 2022
    all permutations of array in two ways

--- permutations.js
+++ permutations.js
@@ -1,8 +1,11 @@
 //Using template to solve problem so we can expend to other problems
 
+
 nums = [1,2,3,4]
 
-var permute = (nums) => {
+//OPTION 1 
+
+const permute = (nums) => {
     //global result
     const result = [];
 
@@ -24,4 +27,42 @@ var permute = (nums) => {
     return result;
 };
 
-console.log(permute(nums))
+// console.log(permute(nums))
+
+
+
+//Heap's Algorithm PURE (but has non-pure functions within)
+// OPTION 2 
+const getPermutation = arr => {
+
+    const output = [];
+
+    //NOT PURE SWAP 
+    const swapInPlace = (arrToSwap, indexA, indexB) => {
+        const temp = arrToSwap[indexA];
+        arrToSwap[indexA] = arrToSwap[indexB];
+        arrToSwap[indexB] = temp;
+    }
+    //RECURSIVE so must deal with (edge) end case
+    const generate = (n, heapArr) => {
+        if(n === 1) {
+            output.push(heapArr.slice())
+            return;
+        }
+        //CALLING RECURSIVELY
+        generate(n -1, heapArr);
+        for (let i =0; i < n - 1; i += 1) {
+            if (n % 2 === 0 ){
+                swapInPlace(heapArr, i, n - 1);
+            } else {
+                swapInPlace(heapArr, 0, n - 1);
+            }
+            generate(n - 1, heapArr);
+        }
+    };
+    generate(arr.length, arr.slice()); 
+    return output;
+};
+
+// console.log(getPermutation(nums))
+

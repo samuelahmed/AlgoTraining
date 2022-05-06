223e21d784cf7d7aba2ae8a4b001cca6e7df55f1
samuelahmed <samueladuval@gmail.com>
2022-05-06 16:29:09 -0700

    Fri May  6 16:28:57 PDT 2022
    Topologicalsort Kahns Algo

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,21 +1,2 @@
-const permute = (nums) => {
-//     //global result
-//     const result = [];
-
-//     //DFS recursilve helper 
-//     const dfs = (i, nums) => {
-//         //base case
-//         if(i === nums.length) {
-//             result.push(nums.slice());
-//             return;
-//         }
-//         // DFS rescursive case
-//         for(let j = i; j < nums.length; j += 1) {
-//             [nums[i], nums[j]] = [nums[j], nums[i]];
-//             dfs(i + 1, nums);
-//             [nums[i], nums[j]] = [nums[j], nums[i]];
-//         }
-//     }
-//     dfs(0, nums);
-//     return result;
-// };
+const result = sortByKahnsAlgorithm(elements);
+// console.log(result);
--- topologicalSort.js
+++ topologicalSort.js
@@ -15,3 +15,70 @@
 // 1) begin with selected nod eand do a DFS to unvisted nodes
 // 2) on recursive callback of the DFS add current nodes to the topological ordering in reverse order 
 
+
+
+//KAHNs ALGORITHM
+
+const elements = [
+    {
+        name: "socks",
+        degree: 0,
+    },
+    {
+        name: "shoes",
+        degree: 2,
+        dependsOn: ["socks", "pants"],
+    },
+    {
+        name: "office",
+        degree: 2,
+        dependsOn: ["shoes", "jacket"],
+    },
+    {
+        name: "shirt",
+        degree: 0,
+    },
+    {
+        name: "jacket",
+        degree: 1,
+        dependsOn: ["shirt"],
+    },
+    {
+        name: "pants",
+        degree: 0,
+    },
+]
+
+const sortByKahnsAlgorithm = (elementsArray) => {
+    const sortedQueue = [];
+    const queue = elementsArray.filter(elementsArray => elementsArray.degree === 0);
+    const array = elementsArray.filter(elementsArray => elementsArray.degree !== 0);
+    // console.log(queue)
+    // console.log(array)
+
+    while(queue.length) {
+        const currentNode = queue.shift()
+        if(!currentNode) {
+            queue.length = 0;
+
+        } else {
+            sortedQueue.push(currentNode);
+            for(let i = 0, len = array.length; i < len; i += 1) {
+                const element = array[i];
+                const hasDependencyOnCurrentNode = element.dependsOn.find(dependency => {
+                    return dependency === currentNode.name;
+                })
+                if (hasDependencyOnCurrentNode) {
+                    array[i].degree += -1;
+                    if (array[i].degree === 0) {
+                        queue.push(array[i])
+                    }
+                }
+            }
+        }
+    }
+    return sortedQueue
+};
+
+const result = sortByKahnsAlgorithm(elements);
+console.log(result);

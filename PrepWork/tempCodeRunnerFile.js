6bb8ebf86143ae9fbb7e82b0afbfb8fee5904c35
samuelahmed <samueladuval@gmail.com>
2022-05-30 17:42:06 -0700

    c

--- PrepWork/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-    for (let node of graph.nodes) {
-        console.log(`Node ${node.value}`)
-        for (let connectedNode of node.edgesList) {
-            console.log(`Node ${node.value} is connected to ${connectedNode.value}`)
-        }
-    }

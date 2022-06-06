58405e015a640b21c7a232a7e9905ef2e851ce32
samuelahmed <samueladuval@gmail.com>
2022-06-06 10:27:53 -0700

    Mon Jun  6 10:27:48 PDT 2022

---
+++ PrepWork/binaryTreeDFS.js
@@ -0,0 +1,11 @@
+//print binary tree using DFS (in order, preorder, and post order)
+
+//DFS 
+//go down towards bottom (left side)(any side is okay as long as you go to bottom first )
+//then go right & cross the tree
+// down then across 
+
+// uses Stack (add / remove from the top)
+//
+
+

344b753a223a0d517dc6790ce553e6013a917238
samuelahmed <samueladuval@gmail.com>
2022-06-06 10:37:58 -0700

    Mon Jun  6 10:37:53 PDT 2022

--- PrepWork/binaryTreeDFS.js
+++ PrepWork/binaryTreeDFS.js
@@ -6,6 +6,40 @@
 // down then across 
 
 // uses Stack (add / remove from the top)
-//
+//add node + children, pop top, 
+// add children + pop top 
+// repeat 
 
 
+
+class Node {
+    constructor(val) {
+        // value being stored + pointers to left / right children
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    };
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right =c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//           a 
+//          / \ 
+//         b   c 
+//        / \   \
+//       d   e   f
+
+
+
+const dfsPrint = 

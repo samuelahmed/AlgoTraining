0e74e88d7c1be5c44451184c52e83c8e6e888666
samuelahmed <samueladuval@gmail.com>
2022-06-27 14:43:50 -0700

    Mon Jun 27 14:43:42 PDT 2022
    written on paper

---
+++ Algorithms/basicIterativeSearch.js
@@ -0,0 +1,17 @@
+const search = (val, arr) => {
+    for (let i=0; i <= arr.length; i += 1) {
+        // console.log(i)
+        if (val === arr[i]) {
+        return 'place' + ' ' + i; 
+        }
+    } 
+    return 'NOT FOUND';
+};
+
+const values = [0, 0, 0, 1, 2, 3, 4, 5, 7, 8, 9]
+
+console.log(search(6, values))
+
+
+
+

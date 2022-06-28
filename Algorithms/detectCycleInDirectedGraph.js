11837145f827f74a966199737d683546f04d5858
samuelahmed <samueladuval@gmail.com>
2022-06-28 15:16:56 -0700

    Tue Jun 28 15:16:35 PDT 2022
    got up to dected cycle in directed graph - did not get that one on tiny note since so big

--- Algorithms/detectCycleInDirectedGraph.js
+++ Algorithms/detectCycleInDirectedGraph.js
@@ -1,68 +1,12 @@
 // Leetcode 207 example course scheduler
 // start with DFS and turn input into adjcency list
-
+//from https://www.youtube.com/watch?v=kDvNzEgGTuQ
 
 //true - NO CYCLE
-numCourses = 2, preRequisitesTrue = [[1,0]] 
-
+numCourses = 2, preRequisitesTrue = [[1,0] ] 
 //false - THERE IS CYCLE (CANNOT FINISH ALL COURSES)
 numCourses = 2, preRequisitesFalse = [[1,0], [0,1]]
 
-// 0: [1]
-// 1: [0]
-
-
-// build ajacency list 
-// const buildAdjecencyList = (n, edges) => {
-//     const adjecencyList = Array.from({length: n}, () => []);
-
-//     for(let edge in edges) {
-//         let [source, destination] = edge;
-//         adjecencyList[source].push(destination);
-//     }
-//     return adjecencyList;
-// };
-
-//build DFS traversal 
-
-// const hasCycleDFS = (node, adjecencyList, visited, arrive, depart) => {
-//     arrive[node] += 1;
-//     visited[node] = true;
-//     for(let neighbor of adjecencyList[node]) {
-//         if(!visited[neighbor]) {
-//             visited[neighbor] = true;
-            
-//             if(hasCycleDFS(neighbor, adjecencyList, visited, arrive, depart)) return true;
-//         } else {
-//             //checking backedge
-//             if(depart[neighbor] === 0) return true; 
-//         }
-//     }
-//     depart[node] += 1;
-//     return false;
-// };
-
-
-// const canFinish = (numCourses, preRequisites) => {
-//     const adjecencyList = buildAdjecencyList(numCourses, preRequisites);
-//     const visited = {};
-//     const arrive = Array.from({length: numCourses}, () => 0);
-//     const depart = Array.from({length: numCourses}, () => 0);
-
-//     for(let vertex = 0; vertex < adjecencyList.length; vertex += 1) {
-//         if(!visited[vertex]) {
-//             if(hasCycleDFS(vertex, adjecencyList, visited, arrive, depart)) return false;
-//         }
-//     }
-//     return true; 
-// };
-
-// console.log(canFinish(preRequisitesTrue))
-
-
-
-
-// ATTEMPT #2 something went wrong 
 // THIS WORKS NOW BUT SOMETHING IS DIFF FROM ATTEMPT ABOVE - FIGURE IT OUT!!!!!
 
 const buildAdjList = (n, edges) => {
@@ -106,5 +50,5 @@ var canFinish = (numCourses, prerequisites) => {
     return true; 
 }
 
-// console.log(canFinish(numCourses, preRequisitesFalse))
+console.log(canFinish(numCourses, preRequisitesFalse))
 // console.log(canFinish(numCourses, preRequisitesTrue))

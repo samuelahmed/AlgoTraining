899b3562007f050b6c6af12a771018276b49ef04
samuelahmed <samueladuval@gmail.com>
2022-07-02 18:50:53 -0700

    Add function maximumWealth

---
+++ Leetcode/Arrays/richestCustomerWealth.js
@@ -0,0 +1,12 @@
+const maximumWealth = (accounts) => {
+    let res = 0;
+
+    for(let i=0; i<accounts.length; i++){
+        let temp = 0;
+        for(let j = 0; j< accounts[i].length; j++){
+            temp += accounts[i][j];
+        }
+        res = Math.max(res, temp);
+    }
+    return res;
+}

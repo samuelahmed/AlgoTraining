80dadb423a514723116327d394ac8763554434ca
samuelahmed <samueladuval@gmail.com>
2022-07-02 18:40:03 -0700

    Add function getConcatenation

---
+++ Leetcode/Arrays/getConcatenation.js
@@ -0,0 +1,10 @@
+
+const num1 = [1,2,3,4,5]
+const num2 = [3,2,3,4,5]
+
+
+var getConcatenation = function (num1, num2) {
+    return [...num1, ...num2]
+}
+
+// console.log(getConcatenation(num1, num2))

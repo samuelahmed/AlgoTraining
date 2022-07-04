b54cd0add2cc6f2e3ad6e41cbb188037aff5dceb
samuelahmed <samueladuval@gmail.com>
2022-07-03 23:46:50 -0700

    add countMatches function

---
+++ Leetcode/Arrays/sumOddLengthSubarrays.js
@@ -0,0 +1,13 @@
+/*/
+https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+1588. Sum of All Odd Length Subarrays
+
+
+Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+A subarray is a contiguous subsequence of the array.
+
+
+/*/
+
+const 

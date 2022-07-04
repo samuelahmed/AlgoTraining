8f37078bf542b8f97c791f87bd6b8c2e0abe220c
samuelahmed <samueladuval@gmail.com>
2022-07-03 23:15:22 -0700

    Add function decompressRLEList

---
+++ Leetcode/Arrays/decompressRLEList.js
@@ -0,0 +1,18 @@
+//1313. Decompress Run-Length Encoded List
+/*/
+We are given a list nums of integers representing a list compressed with run-length encoding.
+
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+Return the decompressed list.
+/*/
+
+const decompressRLEList = (nums) => {
+
+    const result = [];
+
+    for (let i=1; i< nums.length; i+=2){
+        result.push(...new Array(nums[i-1]).fill(nums[i]));
+    }
+    return result;
+}

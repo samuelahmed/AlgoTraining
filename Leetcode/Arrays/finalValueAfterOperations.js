4a1146c0efbc9a386685e4a865edb4549ba7320d
samuelahmed <samueladuval@gmail.com>
2022-07-05 15:47:31 -0700

    notes finalValueAfterOperations

--- Leetcode/Arrays/finalValueAfterOperations.js
+++ Leetcode/Arrays/finalValueAfterOperations.js
@@ -1,3 +1,34 @@
+/*/
+https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+2011. Final Value of Variable After Performing Operations
+
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+ 
+
+
+
+
+NOTES: 
+// initatize final value
+//loop through the operations values (not index) i 
+// if i === ++ increment final value if opeartions ++
+// else decrement 
+//return final value
+
+/*/
+
+
+
+
+
 const finalValueAfterOperations = (operations) => {
     let count = 0;
     for (let i of operations) {

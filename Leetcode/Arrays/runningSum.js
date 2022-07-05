1105b6ba0f0e7d284c30263de660b1b443ea5fb2
samuelahmed <samueladuval@gmail.com>
2022-07-05 16:04:42 -0700

    Notes runningSum

--- Leetcode/Arrays/runningSum.js
+++ Leetcode/Arrays/runningSum.js
@@ -1,3 +1,25 @@
+/*/
+1480. Running Sum of 1d Array
+
+https://leetcode.com/problems/running-sum-of-1d-array/
+
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+
+
+NOTES: 
+//create for loop from 1(i) -> end of array 
+//add i and i-1 in a new array 
+//return new array 
+
+/*/
+
+
+
+
 const runningSum = (nums) => {
     for (let i = 1; i < nums.length; i++){
         nums[i] += nums [i-1]

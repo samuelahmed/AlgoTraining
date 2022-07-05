bc332b382b5ff72bbe79ce4de49a7dc6f0e9b47f
samuelahmed <samueladuval@gmail.com>
2022-07-05 15:59:50 -0700

    Notes getConcatenation

--- Leetcode/Arrays/getConcatenation.js
+++ Leetcode/Arrays/getConcatenation.js
@@ -1,10 +1,22 @@
 
-const num1 = [1,2,3,4,5]
-const num2 = [3,2,3,4,5]
+/*/
+1929. Concatenation of Array
+https://leetcode.com/problems/concatenation-of-array/
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
 
 
-var getConcatenation = function (num1, num2) {
-    return [...num1, ...num2]
+NOTES: 
+//split array twice ...[],...[]
+// return split value
+/*/
+
+
+var getConcatenation = function (nums) {
+    return [...nums, ...nums]
 }
 
 // console.log(getConcatenation(num1, num2))

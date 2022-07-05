78a3129b024e9231d08f023284e8758f516d533c
samuelahmed <samueladuval@gmail.com>
2022-07-05 16:06:20 -0700

    Notes smallerNumbersThanCurrent

--- Leetcode/Arrays/smallerNumbersThanCurrent.js
+++ Leetcode/Arrays/smallerNumbersThanCurrent.js
@@ -1,3 +1,19 @@
+/*/
+1365. How Many Numbers Are Smaller Than the Current Number
+https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+NOTES: 
+/*/
+
+
+
+
+
 var smallerNumbersThanCurrent = function (nums) {
     
     let sorted = nums.slice().sort((a,b) => a-b);

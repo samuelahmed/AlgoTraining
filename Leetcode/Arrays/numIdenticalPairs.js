7379950d0fce669a6792e86605f716c72bff3e10
samuelahmed <samueladuval@gmail.com>
2022-07-05 16:02:37 -0700

    Notes numIdenticalPairs

--- Leetcode/Arrays/numIdenticalPairs.js
+++ Leetcode/Arrays/numIdenticalPairs.js
@@ -1,3 +1,23 @@
+/*/
+1512. Number of Good Pairs
+https://leetcode.com/problems/number-of-good-pairs/
+
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+NOTES:     
+//create an otpout counting good pairs: answer
+// loop through array nums with for loop normally
+//loop through array j=[i+1] of nums
+//compares nums i and j
+//increase count for each === pair 
+
+/*/
+
+
+
+
 var numIdenticalPairs = function (nums) {
 
     let count = 0;

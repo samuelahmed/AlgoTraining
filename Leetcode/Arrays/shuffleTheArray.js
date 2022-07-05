89e4fb2b979e050497dfb1d902c1c4bab12e0c20
samuelahmed <samueladuval@gmail.com>
2022-07-05 16:05:32 -0700

    Notes shuffleArray

--- Leetcode/Arrays/shuffleTheArray.js
+++ Leetcode/Arrays/shuffleTheArray.js
@@ -1,3 +1,30 @@
+/*/
+1470. Shuffle the Array
+https://leetcode.com/problems/shuffle-the-array/
+
+
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+
+
+
+NOTES: 
+//create empty answer array 
+//loop through the n array with i i<n
+//push into answer nums i and i+n
+//return answer 
+
+/*/
+
+
+
+
+
+
+
+
 var shuffle = function (nums, n) {
     let res = [];
 

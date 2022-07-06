86ac35add43bcb7b782792cbd0de5e98c0687266
samuelahmed <samueladuval@gmail.com>
2022-07-05 17:20:48 -0700

    restoreString

--- Leetcode/Arrays/restoreString.js
+++ Leetcode/Arrays/restoreString.js
@@ -5,9 +5,14 @@ https://leetcode.com/problems/shuffle-string/
 
 You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 
-
-
 Return the shuffled string.
+
+NOTES: 
+//initialize empty results array
+// loop through indices 1,2,3,4,5,6,7
+// tie the indices result to string  result[indices[i]] = s[i]
+// return the final output with joined result (if joined earlier it doesn't work will be array with 7 items.)
+
 
 /*/
 

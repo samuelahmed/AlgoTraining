dbf4745216865958507469ca929618dc53598f99
samuelahmed <samueladuval@gmail.com>
2022-07-09 18:28:33 -0700

    c

--- Leetcode/Arrays/shuffleTheArray.js
+++ Leetcode/Arrays/shuffleTheArray.js
@@ -18,15 +18,17 @@ NOTES:
 
 /*/
 
-
+ 
 
 
 var shuffle = function (nums, n) {
+
     let res = [];
 
     for(let i = 0; i < n; i++){
         res.push(nums[i])
         res.push(nums[i+n])
     }
+
     return res
 };

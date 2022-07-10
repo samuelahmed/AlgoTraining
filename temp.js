09a70bfaa2a3475e4eaa9740141613b1392d70d3
samuelahmed <samueladuval@gmail.com>
2022-07-10 14:50:26 -0700

    map examples

--- temp.js
+++ temp.js
@@ -24,6 +24,23 @@ let split = splitString.split(',')
 const fillArray = [0,1,2,3,4,5]
 const fillArray2 = [0,1,2,3,4,5]
 fillArray.fill(0)
-[ 0, 0, 0, 0, 0, 0 ]
+// [ 0, 0, 0, 0, 0, 0 ]
 fillArray2.fill(0,3,5)
-[ 0, 1, 2, 0, 0, 5 ]
+// [ 0, 1, 2, 0, 0, 5 ]
+
+
+//simple map example * 2
+const numbers = [1,2,3,4,5];
+let mapNumbers = numbers.map(nums => (nums * 2))
+console.log(mapNumbers)
+//[ 2, 4, 6, 8, 10 ]
+
+
+//map to turn string number into reg number with Number operator
+const stringNumbers = ['1', '2', '3', '4', '5']
+const regNumbers = stringNumbers.map(Number)
+console.log(regNumbers)
+// [ 1, 2, 3, 4, 5 ]
+
+
+//

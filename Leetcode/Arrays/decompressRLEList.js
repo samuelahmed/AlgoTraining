0f58a77bc7bb18aafb257b0fb76931e4f124ae8a
samuelahmed <samueladuval@gmail.com>
2022-07-09 23:44:16 -0700

    Steps decompressRLEList

--- Leetcode/Arrays/decompressRLEList.js
+++ Leetcode/Arrays/decompressRLEList.js
@@ -6,6 +6,15 @@ We are given a list nums of integers representing a list compressed with run-len
 Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
 
 Return the decompressed list.
+
+1. initialize empty result array
+2. loop through nums, starting from 1, increasing by 2 each time
+3. push result into a new array 
+4. fill new array with nums[i]
+5. return result 
+
+
+
 /*/
 
 const decompressRLEList = (nums) => {

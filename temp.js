bde3b9bfca79f0c99ad8084645ec1925b836174b
samuelahmed <samueladuval@gmail.com>
2022-07-10 14:41:41 -0700

    fillArray

--- temp.js
+++ temp.js
@@ -9,5 +9,21 @@ console.log(names)
 
 const array = [1,2,3,4,5]
 let reduceExample = array.reduce((callback, currentValue) => callback + currentValue)
-console.log(reduceExample)
+// console.log(reduceExample)
 //output: 15
+
+
+
+const splitString = 'Hello,my,name,is,Sam'
+let split = splitString.split(',')
+// console.log(split) 
+//[ 'Hello', 'my', 'name', 'is', 'Sam' ]
+
+
+
+const fillArray = [0,1,2,3,4,5]
+const fillArray2 = [0,1,2,3,4,5]
+fillArray.fill(0)
+[ 0, 0, 0, 0, 0, 0 ]
+fillArray2.fill(0,3,5)
+[ 0, 1, 2, 0, 0, 5 ]

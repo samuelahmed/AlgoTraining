342094563cf487898225680a5ce416f53821e94b
samuelahmed <samueladuval@gmail.com>
2022-07-11 13:03:00 -0700

    notes smallerNumbersThanCurrent

--- Leetcode/Arrays/smallerNumbersThanCurrent.js
+++ Leetcode/Arrays/smallerNumbersThanCurrent.js
@@ -9,23 +9,37 @@ Return the answer in an array.
 
 NOTES: 
 
+1. Initialize empty results array
+2. Initialie sorted array
+3. Slice() sorted array to duplicated
+4. Sort() sorted array using (a,b) => a-b so it works for numbers 
+5. for loop through nums
+6. Compare the value of sorted array with nums array and push into results the indexOf at which sorted is in nums[i]
+7. Return results 
+
 
 
 /*/
-
-
-
-
-
-var smallerNumbersThanCurrent = function (nums) {
+// var smallerNumbersThanCurrent = function (nums) {
     
-    let sorted = nums.slice().sort((a,b) => a-b);
+//     let sorted = nums.slice().sort((a,b) => a-b);
 
-    let result = [];
+//     let result = [];
 
-    for (let i = 0; i < nums.length; i++) {
+//     for (let i = 0; i < nums.length; i++) {
+//         result.push(sorted.indexOf(nums[i]))
+//     }
+
+//     return result
+// }
+
+const smallerNumbersThanCurrent = (nums) => {
+    results = []
+
+    sorted = nums.slice().sort((a,b)=>a-b)
+
+    for(let i = 0; i<nums.length; i++){
         result.push(sorted.indexOf(nums[i]))
     }
-
-    return result
+    return results
 }

d5a63bd5a53d1196d4eacd725b465354b69d0ca8
samuelahmed <samueladuval@gmail.com>
2022-07-20 16:43:23 -0700

    c

---
+++ Leetcode/Hash-Tables/divideArray.js
@@ -0,0 +1,31 @@
+/*
+2206. Divide Array Into Equal Pairs
+
+https://leetcode.com/problems/divide-array-into-equal-pairs/
+
+
+You are given an integer array nums consisting of 2 * n integers.
+
+You need to divide nums into n pairs such that:
+
+Each element belongs to exactly one pair.
+The elements present in a pair are equal.
+Return true if nums can be divided into n pairs, otherwise return false.
+
+STEPS:
+
+
+
+*/
+
+
+const divideArray = (nums) => {
+    
+    let map = new Map();
+      
+    for (const number of nums) {
+      map.has(number) ? map.delete(number) : map.set(number, true);
+    }
+      
+    return map.size === 0;
+  };

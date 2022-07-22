e2140e7f6a4a6389d68736f2da7a5bd3d22812a5
samuelahmed <samueladuval@gmail.com>
2022-07-22 16:19:55 -0700

    c

--- Leetcode/Hash-Tables/isIsomorphic.js
+++ Leetcode/Hash-Tables/isIsomorphic.js
@@ -1,9 +1,16 @@
 /*
+205. Isomorphic Strings
+https://leetcode.com/problems/isomorphic-strings/
 
 
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
 
 
-
+STEPS:
 
 
 
@@ -14,7 +21,7 @@ const isIsomorphic = (s, t) => {
     
     let map = new Map()
 
-    for(let i = 0; i<s.length; i++){
+    for (let i = 0; i<s.length; i++){
         if(!map['s' + s[i]]) map['s' + s[i]] = t[i]
         if(!map['t' + t[i]]) map['t' + t[i]] = s[i]
         if(t[i] != map['s' + s[i]] || s[i] != map['t' + t[i]]){  

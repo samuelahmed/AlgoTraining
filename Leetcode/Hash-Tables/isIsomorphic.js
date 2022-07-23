082e0a6735ac56a7581108157143e2b0a745ac37
samuelahmed <samueladuval@gmail.com>
2022-07-23 16:30:36 -0700

    c

--- Leetcode/Hash-Tables/isIsomorphic.js
+++ Leetcode/Hash-Tables/isIsomorphic.js
@@ -26,8 +26,12 @@ const isIsomorphic = (s, t) => {
     let map = new Map()
 
     for (let i = 0; i<s.length; i++){
-        if(!map['s' + s[i]]) map['s' + s[i]] = t[i]
-        if(!map['t' + t[i]]) map['t' + t[i]] = s[i]
+        if(!map['s' + s[i]]) {
+            map['s' + s[i]] = t[i]
+        } 
+        if(!map['t' + t[i]]) {
+            map['t' + t[i]] = s[i]
+        }
         if(t[i] != map['s' + s[i]] || s[i] != map['t' + t[i]]){  
             return false
         } 

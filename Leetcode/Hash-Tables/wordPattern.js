8f67a565fd5e574f98512fce5c7e35e847e5579c
samuelahmed <samueladuval@gmail.com>
2022-07-23 16:30:40 -0700

    c

--- Leetcode/Hash-Tables/wordPattern.js
+++ Leetcode/Hash-Tables/wordPattern.js
@@ -10,14 +10,20 @@ Given a pattern and a string s, find if s follows the same pattern.
 Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
 
 STEPS:
-
-
+Set words to s.split(' ')
+Set map to new Map()
+If words.length is not euqal to pattern return false (catches diff length)
+If words.size is not equal to set(patter).size return false (catches equal word)
+For loop through pattern length 
+If map pattern has pattern [i] and it is not equal to words[i] return false
+Map.set pattern[i]  and words[i]
+Return true
 
 */
 
 
 
-var wordPattern = function(pattern, s) {
+const wordPattern = (pattern, s) => {
     
     let words = s.split(' ')
     let map = new Map()

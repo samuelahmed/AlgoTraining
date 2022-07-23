d7a27bfc44e8dd7fd3244cf665a74f47f83dcc1a
samuelahmed <samueladuval@gmail.com>
2022-07-22 17:24:08 -0700

    c

---
+++ Leetcode/Hash-Tables/wordPattern.js
@@ -0,0 +1,16 @@
+/*
+290. Word Pattern
+
+
+https://leetcode.com/problems/word-pattern/
+
+
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+STEPS:
+
+
+
+*/

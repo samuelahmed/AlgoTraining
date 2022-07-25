d6624712f0de827542970087b0e0f100e7fe4917
samuelahmed <samueladuval@gmail.com>
2022-07-25 13:24:58 -0700

    c

--- Leetcode/Dynamic Programming/countBits.js
+++ Leetcode/Dynamic Programming/countBits.js
@@ -6,6 +6,13 @@ https://leetcode.com/problems/counting-bits/
 
 Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
+STEPS:
+Initalize result array at [0]
+Initialize offset at 1
+For loop through n starting at 1, going till n+1
+If offset * 2 === i then set offset to i 
+result[i] is equal to 1 + result [i - offset]
+Return result
 */
 
 

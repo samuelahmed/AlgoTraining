74ee9e46c36fdc8f08580f92b7ad3dfacb65fd72
samuelahmed <samueladuval@gmail.com>
2022-07-25 20:28:30 -0700

    c

--- Leetcode/Dynamic Programming/generate.js
+++ Leetcode/Dynamic Programming/generate.js
@@ -1,7 +1,21 @@
 /*
+118. Pascal's Triangle
+https://leetcode.com/problems/pascals-triangle/
 
 
+Given an integer numRows, return the first numRows of Pascal's triangle.
 
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+STEPS: 
+Initialize empty results array
+For loop through n 
+Set result[i] to empty array 
+Set result[i][0] equal to result[i][i] = 1
+For loop through i starting at 1
+Set result[i][j] equal to result[i-1][j] + result[i-1][j-1]
+Return result
 
 
 */ 
@@ -10,16 +24,19 @@ const generate = (n) => {
     
     let result = []
     
-    for(let i=0;i<n;i++){
-        result[i]=[]
-        result[i][0] = result[i][i]=1
-        for(let j=1;j<i;j++){
-            result[i][j] = result[i-1][j]+result[i-1][j-1]
+    for(let i = 0; i<n; i++){
+        result[i] = []
+        result[i][0] = result[i][i] = 1
+        for(let j = 1; j < i; j++){
+            result[i][j] = result[i-1][j] + result[i-1][j-1]
         }
     }
     
     return result
 };
+
+
+
 
 
 const generate2 = (n) => {

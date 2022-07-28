c2170170c6615246122bf925a435cba5b18722f3
samuelahmed <samueladuval@gmail.com>
2022-07-27 22:40:08 -0700

    c

---
+++ Leetcode/DFS/maxDepth.js
@@ -0,0 +1,19 @@
+/*
+
+
+
+
+NOTES: 
+
+
+*/
+
+
+const maxDepth = (root) => {
+    
+    if(root === undefined || root === null){
+        return 0
+    }
+    
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+};

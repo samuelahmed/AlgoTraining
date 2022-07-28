5074bb9b72b0ebee781c92e1c9e4b336eb9db185
samuelahmed <samueladuval@gmail.com>
2022-07-28 15:51:17 -0700

    c

--- Leetcode/DFS/rangeSumBST.js
+++ Leetcode/DFS/rangeSumBST.js
@@ -7,8 +7,13 @@ Given the root node of a binary search tree and two integers low and high, retur
 
 
 NOTES: 
-
-
+Initialize result as zero
+If no root return result 
+If root.val is greater than or equal high && root.val is greater than or equal to low 
+Result += root val
+If root.val is greater than low add recusive root.left, low, high
+If root.val is lower than high add recursive root.right, low, high
+Return result
 */
 
 
@@ -16,19 +21,19 @@ NOTES:
 
 
 const rangeSumBST = (root, low, high) => {
-    
-    let result = 0
-    
-    if(!root) return result
-    if(root.val <= high && root.val >= low){
-       result += (root.val)
+
+    let result = 0;
+
+    if (!root) return result;
+    if (root.val <= high && root.val >= low) {
+        result += (root.val);
     }
-    if(root.val > low){
-        result += rangeSumBST(root.left, low, high)
+    if (root.val > low) {
+        result += rangeSumBST(root.left, low, high);
     }
-    if(root.val < high){
-        result += rangeSumBST(root.right, low, high)
+    if (root.val < high) {
+        result += rangeSumBST(root.right, low, high);
     }
-    
-    return result
+  
+    return result;
 };

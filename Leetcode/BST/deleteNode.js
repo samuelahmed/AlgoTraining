72d691a5e872f93b4b324e49171cb00632fa4d30
samuelahmed <samueladuval@gmail.com>
2022-08-02 15:59:44 -0700

    c

---
+++ Leetcode/BST/deleteNode.js
@@ -0,0 +1,43 @@
+/*
+450. Delete Node in a BST
+https://leetcode.com/problems/delete-node-in-a-bst/
+
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+
+
+STEPS:
+
+
+
+
+*/
+
+
+
+
+
+const deleteNode = (root, key) => {
+    
+    const callDFS = (node) => {
+        if(!node) return null;
+        if(node.val === key) {
+            if(!node.left) return node.right;
+            if(!node.right) return node.left;
+            let curr = node.right;
+            while(curr.left) curr = curr.left;
+            curr.left = node.left;
+            return node.right;
+        }
+        if(key > node.val) node.right = callDFS(node.right);
+        else node.left = callDFS(node.left);
+        
+        return node;
+    };
+    
+    return callDFS(root);
+};

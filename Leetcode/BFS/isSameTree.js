d2b32c497b89f5ccc6e4c53ae9fcba7bc0890c2e
samuelahmed <samueladuval@gmail.com>
2022-08-02 12:47:04 -0700

    c

--- Leetcode/BFS/isSameTree.js
+++ Leetcode/BFS/isSameTree.js
@@ -1,4 +1,11 @@
 /*
+
+100. Same Tree
+
+https://leetcode.com/problems/same-tree/
+
+
+
 If no p and q return true
 if no p or queue, or p.val is not equal to q.val return false
 returse recursive isSameTree(p.left and q.left) && isSameTree(p.right and q.right)
@@ -16,4 +23,4 @@ const isSameTree = (p, q) => {
     }
 
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-}
+};

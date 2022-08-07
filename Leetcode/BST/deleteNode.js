e5f1a8ff2fd2be605e57c2b530c788af6af09f75
samuelahmed <samueladuval@gmail.com>
2022-08-06 21:36:00 -0700

    c

--- Leetcode/BST/deleteNode.js
+++ Leetcode/BST/deleteNode.js
@@ -10,6 +10,9 @@ Search for a node to remove.
 If the node is found, delete the node.
 
 
+
+
+
 STEPS:
 
 
@@ -19,7 +22,30 @@ STEPS:
 
 
 
-
+const deleteNode = (root, key) => {
+    
+    const callDFS = (node) => {
+        if (!node) return null;
+        if (node.val === key) {
+            if (!node.left) return node.right;
+            if (!node.right) return node.left;
+            let curr = node.right;
+            while (curr.left) {
+                curr = curr.left;
+            }
+            curr.left = node.left
+            return node.right;
+        }
+        if (key > node.val) {
+            node.right = callDFS(node.right);
+        } else {
+            node.left = callDFS(node.left);
+        }
+        return node;
+    };
+    
+    return callDFS(root);
+};
 
 const deleteNode = (root, key) => {
     

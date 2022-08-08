896c756e75b2446ab47d3a6cb307dc970a9f7027
samuelahmed <samueladuval@gmail.com>
2022-08-08 12:28:45 -0700

    Steps for numTrees

--- Leetcode/BST/numTrees.js
+++ Leetcode/BST/numTrees.js
@@ -4,12 +4,13 @@ https://leetcode.com/problems/unique-binary-search-trees/
 Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
 
 STEPS:
-
-
-
+Initialize result as new array n + 1 and fill with 0 
+Set result[0] to 1 and result[1] to 1
+For loop starting from 2 lessthen or equal to n
+For loop stating from 1 lessthen or equal to i 
+Set result[i] to be result[j - 1] times result[i - j]
+Return result[n]
 */
-
-
 
 
 //With dynamic programming

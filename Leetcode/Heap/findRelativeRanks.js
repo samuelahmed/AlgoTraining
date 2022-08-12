fe5279aa9f4eaceed92f8be49e9b1eb999f80e5b
samuelahmed <samueladuval@gmail.com>
2022-08-11 17:42:12 -0700

    cc

--- Leetcode/Heap/findRelativeRanks.js
+++ Leetcode/Heap/findRelativeRanks.js
@@ -19,31 +19,31 @@ STEPS:
 
 //Assuming PriorityQueue Library Available
 const findRelativeRanks = function (score) {
-	const ranks = new MaxPriorityQueue();
     
-	for (let i = 0; i < score.length; i++)
-		ranks.enqueue(i, score[i]);
+    let ranks = new MaxPriorityQueue();
 
-	for (let i = 0; i < score.length; i++) {
-		const item = ranks.dequeue();
-		let rank;
-        
-		switch (i) {
-			case 0:
-				rank = "Gold Medal";
-				break;
-			case 1:
-				rank = "Silver Medal";
-				break;
-			case 2:
-				rank = "Bronze Medal";
-				break;
-			default:
-				rank = (i + 1).toString();
-		}
-        
-		score[item.element] = rank;
-	}
+    for (let i = 0; i < score.length; i++) {
+        ranks.enqueue(i, score[i]);
+    }
+        for (let i = 0; i < score.length; i++) {
+            const item = ranks.dequeue();
+            let rank;
+            
+            switch (i) {
+                case 0:
+                    rank = "Gold Medal";
+                    break;
+                case 1:
+                    rank = "Silver Medal";
+                    break;
+                case 2:
+                    rank = "Bronze Medal";
+                    break;
+                default:
+                    rank = (i + 1).toString();
+            }
+        score[item.element] = rank;
+        }
     
-	return score;
+    return score;
 };
--- Leetcode/Stack/findBuildings.js
+++ Leetcode/Stack/findBuildings.js
@@ -17,14 +17,14 @@ STEPS:
 
 const findBuildings = (heights) => {
     
-    const stack = []
+    const stack = [];
     
     for (let i = 0; i < heights.length; i++) {
         while (stack.length && heights[i] >= heights[stack[stack.length - 1]]) {
-            stack.pop()
+            stack.pop();
         }
-        stack.push(i)
+        stack.push(i);
     }
     
-    return stack
-}
+    return stack;
+};

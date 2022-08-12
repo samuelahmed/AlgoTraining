b9cf841ee2c79e2cb70608226b46bba350f936b1
samuelahmed <samueladuval@gmail.com>
2022-08-12 15:50:47 -0700

    add arithmeticTriplets

---
+++ Leetcode/Two Pointers/arithmeticTriplets.js
@@ -0,0 +1,34 @@
+/*
+2367. Number of Arithmetic Triplets
+https://leetcode.com/problems/number-of-arithmetic-triplets/
+
+
+
+You are given a 0-indexed, strictly increasing integer array nums and a positive integer diff. A triplet (i, j, k) is an arithmetic triplet if the following conditions are met:
+
+i < j < k,
+nums[j] - nums[i] == diff, and
+nums[k] - nums[j] == diff.
+Return the number of unique arithmetic triplets.
+
+STEPS: 
+
+*/
+
+
+const arithmeticTriplets = (nums, diff) => {
+    
+    let object = {};
+    let result = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= diff * 2){
+        if (object[nums[i]-diff] && object[nums[i] - diff * 2]) {
+                result++
+            }
+        }   
+    object[nums[i]] = true;
+    }
+    
+    return result;
+};
